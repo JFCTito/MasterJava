34a559fd08b990b29a0326a4e5f82e6065686551
tito <jfctito@gmail.com>
2024-05-14 20:52:00 +0200

    correction EjemploAsignarPropiedadesDeSistema Class

--- src/EjemploAsignarPropiedadesDeSistema.java
+++ src/EjemploAsignarPropiedadesDeSistema.java
@@ -17,7 +17,8 @@ public class EjemploAsignarPropiedadesDeSistema {
             ps.list(System.out);
 
         } catch (Exception e){
-            System.out.println("No existe el archivo = " + e);
+            System.err.println("No existe el archivo = " + e);
+            System.exit(1);
         }
     }
 }
