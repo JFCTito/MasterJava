2f0ba5c95c64c57562882311ec13f4fd42e699b0
tito <jfctito@gmail.com>
2024-05-14 21:39:15 +0200

    EjemploClaseMath Class modification

--- src/EjemploClaseMath.java
+++ src/EjemploClaseMath.java
@@ -22,6 +22,26 @@ public class EjemploClaseMath {
         long entero = Math.round(3.5);
         System.out.println("entero = " + entero);
 
+        double exp = Math.exp(2);
+        System.out.println("exp = " + exp);
 
+        double log = Math.log(10);
+        System.out.println("log = " + log);
+
+        double potencia = Math.pow(10, 3);
+        System.out.println("potencia = " + potencia);
+
+        double raiz = Math.sqrt(5);
+        System.out.println("raiz = " + raiz);
+
+        double grados = Math.toDegrees(1.57);
+        grados = Math.round(grados);
+        System.out.println("grados = " + grados);
+
+        double radianes = Math.toRadians(90.00);
+        System.out.println("radianes = " + radianes);
+
+        System.out.println("sin(90): " + Math.sin(radianes));
+        System.out.println("cos(90): " + Math.cos(radianes));
     }
 }
