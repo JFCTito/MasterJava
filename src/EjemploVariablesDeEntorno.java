6aa857394ed587b475eb41a07ccd507004fa00e0
tito <jfctito@gmail.com>
2024-05-14 20:41:56 +0200

    EjemploVariablesDeEntorno Class

---
+++ src/EjemploVariablesDeEntorno.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+
+public class EjemploVariablesDeEntorno {
+    public static void main(String[] args) {
+
+        Map<String, String> varEnv = System.getenv();
+        System.out.println("variables de entorno del sistema = " + varEnv);
+
+        String username = System.getenv("USERNAME");
+        System.out.println("username = " + username);
+
+        String javaHome = System.getenv("JAVA_HOME");
+        System.out.println("javaHome = " + javaHome);
+
+        String tempDir = System.getenv("TEMP");
+        System.out.println("tempDir = " + tempDir);
+
+        String path = System.getenv("Path");
+        System.out.println("path = " + path);
+    }
+}
