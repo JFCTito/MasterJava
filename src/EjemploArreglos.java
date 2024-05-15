e7e56f8caa220ae96ce90923422d2bdec2a74abd
tito <jfctito@gmail.com>
2024-05-15 17:39:31 +0200

    EjemploArreglos Class modified

--- src/EjemploArreglos.java
+++ src/EjemploArreglos.java
@@ -1,10 +1,28 @@
+import java.util.Arrays;
+
 public class EjemploArreglos {
     public static void main(String[] args) {
+
+        String[] productos = new String[5];
+
+        productos[0] = "Tabla";
+        productos[1] = "Zapato";
+        productos[2] = "Laptop";
+        productos[3] = "Teclado";
+        productos[4] = "Mouse";
+
+        Arrays.sort(productos);
+
+        System.out.println("productos[0] = " + productos[0]);
+        System.out.println("productos[1] = " + productos[1]);
+        System.out.println("productos[2] = " + productos[2]);
+        System.out.println("productos[3] = " + productos[3]);
+        System.out.println("productos[4] = " + productos[4]);
 
         int[] numeros = new int[4];
 
         numeros[0] = 1;
-        numeros[1] = Integer.valueOf("2");
+        numeros[1] = Integer.parseInt("2");
         numeros[2] = 3;
         numeros[3] = 4;
 
